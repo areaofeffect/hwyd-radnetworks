2f7d0da1f2c718c3ebed5d944d6538f65e8f406e
Carrie Kengle <carriesteady@gmail.com>
2015-10-22 23:21:20 -0400

    step3

--- hwyd.js
+++ hwyd.js
@@ -2,22 +2,26 @@ Router.configure({
   layoutTemplate: 'ApplicationLayout'
 });
 
+Router.route('/', function () {
+  this.render('Home')
+});
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+  var sendColor = function (arg) {
+    var color = "#"+$.colpick.hsbToHex(arg);
+    console.log("color", color);
+  }
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+  Template.home.rendered = function () {
+    $('#picker').colpick({
+      flat:true,
+      layout:'hex',
+      submit:true,
+      submitText: "GO",
+      onSubmit: sendColor
+    })
+  };
 }
 
 if (Meteor.isServer) {
--- steps.md
+++ steps.md
@@ -7,3 +7,13 @@ Add some routes
 	meteor add iron:router
 	meteor add twbs:bootstrap
 	
+#STEP3
+Add home page content
+
+1. Add colpick CSS/JS to the public folder
+
+2. Create a send color function
+
+3. Intialize the colpick when the home template is rendered
+
+	
